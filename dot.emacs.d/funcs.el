946e8acc1a99bf75658320b14ef66d08bce9ed5a
Zach Davis <Zach.Davis@osii.com>
2014-03-26 08:29:27 -0500

    Re-formatted code, added some functions

--- dot.emacs.d/funcs.el
+++ dot.emacs.d/funcs.el
@@ -1,6 +1,10 @@
 ;;; funcs.el --- Handy elisp functions
 ;; -*- mode: emacs-lisp-mode; -*-
 
+
+;;; Commentary:
+;; Handy elisp functions.
+
 ;;; Code:
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -11,8 +15,7 @@
   (interactive)
   (message "Converting line endings to dos(CRLF). **Save buffer to commit")
   (setq buffer-file-coding-system 'undecided-dos)
-  (not-modified t)
-)
+  (set-buffer-modified-p t))
 
 (defun dos2unix ()
   "Replace CR's with nothing and set encoding to unix."
@@ -21,55 +24,45 @@
   (save-excursion
     (goto-char (point-min))
     (while (re-search-forward "\x0d" nil t)
-        (replace-match "")))
+      (replace-match "")))
   (setq buffer-file-coding-system 'undecided-unix)
-  (not-modified t)
-)
+  (set-buffer-modified-p t))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Insert template when certain files are created.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq template-files-location "~/Templates/")
-(setq ext-template-map '(("py"   . "Py File.py")
-			 ("c"    . "C File.c")
-			 ("html" . "HTML File.html")
-			 ("java" . "Java File.java")) )
+(defvar template-files-dir "~/Templates/")
+(defvar ext-template-map '(("py"   . "Py File.py")
+						   ("c"    . "C File.c")
+						   ("html" . "HTML File.html")
+						   ("java" . "Java File.java")))
 
 (defun insert-template()
-  "ask user to insert template certain file creations"
-  (let (ext template)
-	(setq ext (car (cdr (split-string (buffer-name) "\\."))))
-	(setq template (cdr (assoc ext ext-template-map)))
-	  (if template
-	    (progn
-              (if (string= (buffer-string) "")
-	      (progn
-              (if (yes-or-no-p (concat "Insert template for " ext " file? "))
-	          (insert-file (concat template-files-location template)))))))))
+  "Ask user to insert a file template when a new blank buffer is created
+whose extense matches one of the available templates."
+  (let* ((ext (cadr (split-string (buffer-name) "\\.")))
+         (template (cdr (assoc ext ext-template-map))))
+    (when (and template (string= (buffer-string) ""))
+      (if (yes-or-no-p (concat "Insert template for " ext " file? "))
+          (insert-file-contents (concat template-files-dir template))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; rm-wsp - extraneous whitespace remover
+;; -> deprecated - use whitespace-cleanup instead
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun rm-wsp ()
-  "Remove whitespace from blank lines, and ends of lines."
-  (interactive)
-  (save-excursion
-    (goto-char (point-min))
-    (while (re-search-forward "\\s-+$" nil t)
-        (replace-match "")
-	)
-
-    (goto-char (point-min))
-    (while (re-search-forward "^[:space:]+$" nil t)
-        (replace-match "")
-	)
-  )
-  (message "Extraneous whitespace removed")
-)
+;(defun rm-wsp ()
+;  "Remove whitespace from blank lines, and ends of lines."
+;  (interactive)
+;  (save-excursion
+;    (goto-char (point-min))
+;    (while (re-search-forward "[[:space:]]+$" nil t)
+;      (replace-match "\n")))
+;  (message "Extraneous whitespace removed"))
+(defalias 'rm-wsp 'whitespace-cleanup)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
@@ -80,13 +73,11 @@
   "Spit out my name and email."
   (interactive)
   (insert (concat "~" user-full-name " - " user-mail-address)))
-(global-set-key [f1]  'signature)
 
 (defun datestamp ()
   "Print out the current date mm-dd-yyyy."
   (interactive)
   (insert (format-time-string "%m-%d-%Y")))
-(global-set-key [f2]  'datestamp)
 
 (defun datestamp-fancy ()
   "Print out the current date: month day, year."
@@ -125,153 +116,93 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defadvice comment-region (before comment-line-if-no-region activate compile)
   "Comment region, or line if no region."
-  (interactive)
-  (if mark-active (list (region-beginning) (region-end))
-	(list (line-beginning-position) (line-beginning-position 2))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Change python mode's comment-dwim to
-;;  comment out line if no region is selected
-;;  rather than add an end-of-line comment.
-;;
-;;  this could be a defadvice, but sometimes i like the
-;;  default behavior...
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun comment-dwim-line-if-no-region ()
-  (interactive)
-  (if mark-active (list (region-beginning) (region-end))
-                  (list (line-beginning-position) (line-beginning-position 2)))
-  (lambda () (comment-dwim)))
+  (interactive (if mark-active (list (region-beginning) (region-end))
+    (list (line-beginning-position) (line-beginning-position 2)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; diff-region* - Diff two regions
 ;;
-;;  To compare two regions, select the first
-;; region and run `diff-region`.  The region is
-;; now copied to a seperate diff-ing buffer.
-;; Next, navigate to the next region in question
-;; (even in another file).  Mark the region and
-;; run `diff-region-now`, the diff of the two
-;; regions will be displayed by ediff.
+;;  To compare two regions, select the first region and run
+;; `diff-region'.  The region is copied to a temporary buffer.  Next,
+;; navigate to the next region in question (even in another buffer or
+;; file).  Mark the region and run `diff-region-now', the diff of the
+;; two regions will be displayed by `ediff'.
 ;;
-;;  You can re-select the first region at any time
-;; by re-calling `diff-region`.
+;;  You can re-select the first region at any time by re-calling
+;; `diff-region' again.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun diff-region ()
-  "Select a region to compare."
-  (interactive)
-  (when (use-region-p)  ; there is a region
-		(let (buf)
-		  (setq buf (get-buffer-create "*Diff-regionA*"))
-		  (save-current-buffer
-			(set-buffer buf)
-			(erase-buffer))
-		  (append-to-buffer buf (region-beginning) (region-end)))
-  )
-  (message "Now select other region to compare and run `diff-region-now`")
-)
+(defun diff-region (start end)
+  "Select a region to compare.
+Should be followed by a call to  `diff-region-now'.
+Argument START Start of first region.
+Argument END End of first region."
+  (interactive "r")
+  (let ((buf (get-buffer-create "*Diff-regionA*")))
+	(with-current-buffer buf (erase-buffer))
+	(append-to-buffer buf start end))
+  (message "Select second region to compare and run `diff-region-now'"))
 
-(defun diff-region-now ()
-  "Compare current region with region already selected by `diff-region'."
-  (interactive)
-  (when (use-region-p)
-		(let (bufa bufb)
-		  (setq bufa (get-buffer-create "*Diff-regionA*"))
-		  (setq bufb (get-buffer-create "*Diff-regionB*"))
-		  (save-current-buffer
-			(set-buffer bufb)
-			(erase-buffer))
-		  (append-to-buffer bufb (region-beginning) (region-end))
-		  (ediff-buffers bufa bufb))
-  )
-)
-
+(defun diff-region-now (start end)
+  "Compares current region with region selected by `diff-region'.
+Argument START Start of second region.
+Argument END End of second region."
+  (interactive "r")
+  (let ((bufa (get-buffer-create "*Diff-regionA*"))
+		(bufb (get-buffer-create "*Diff-regionB*")))
+	(with-current-buffer bufb (erase-buffer))
+	(append-to-buffer bufb start end)
+	(ediff-buffers bufa bufb)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Duplcate line down
-;;
-;; Copy the current line and paste a copy on a new line below the
-;; current one
-;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun duplicate-line ()
   "Duplicate the current line."
   (interactive)
   (let* ((pos-end (line-beginning-position 2))
          (line    (buffer-substring (line-beginning-position) pos-end)))
-	(save-excursion
-	  (goto-char pos-end)
-	  (unless (bolp) (newline))
-	  (insert line))
-	(next-line)))
+    (save-excursion
+      (goto-char pos-end)
+      (unless (bolp) (newline))
+      (insert line))
+    (forward-line)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; unescape xml buffer
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun xml-unescape-buffer ()
-  "Un-escape a buffer of escaped XML."
-  (interactive)
+(defun xml-unescape-buffer (start end)
+  "Un-escape a buffer of escaped XML.
+Argument START Start of region to un-escape.
+Argument END End of escaped region."
+  (interactive "r")
 ;  (save-excursion
-  (let (m1 rep)
-	(while (search-forward-regexp "&\\(lt\\|gt\\|#xD\\|amp\\|quot\\|apos\\);" nil t)
-	  (progn
-		(setq m1 (match-string 1))
-		(message m1)
-		(setq rep (assoc m1 xml-escapes))
-		(when rep
-		  (replace-match (cdr rep) nil t))))))
+  (goto-char start)
+  (while (search-forward-regexp "&\\(lt\\|gt\\|#xD\\|amp\\|quot\\|apos\\);" end t)
+	(let* ((m1 (match-string 1))
+		   (rep (assoc m1 xml-escapes)))
+	  (when rep
+		(replace-match (cdr rep) nil t)))))
 
 (defvar xml-escapes
-  '(("#xD" . "") ; technically newline...
-	("gt" . ">")
-	("lt" . "<")
-	("quot" . "\"")
-	("apos" . "'")
-	("amp" . "&")))
+  '(("#xD"  . "") ; technically newline...
+    ("gt"   . ">")
+    ("lt"   . "<")
+    ("quot" . "\"")
+    ("apos" . "'")
+    ("amp"  . "&")))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; taken from stackoverflow, make #if 0...#else
-;;  a c-mode comment
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun c-mode-font-lock-if0 (limit)
-  (save-restriction
-    (widen)
-    (save-excursion
-      (goto-char (point-min))
-      (let ((depth 0) str start start-depth)
-        (while (re-search-forward "^\\s-*#\\s-*\\(if\\|else\\|endif\\)" limit 'move)
-          (setq str (match-string 1))
-          (if (string= str "if")
-              (progn
-                (setq depth (1+ depth))
-                (when (and (null start) (looking-at "\\s-+0"))
-                  (setq start (match-end 0)
-                        start-depth depth)))
-            (when (and start (= depth start-depth))
-              (c-put-font-lock-face start (match-beginning 0) 'font-lock-comment-face)
-              (setq start nil))
-            (when (string= str "endif")
-              (setq depth (1- depth)))))
-        (when (and start (> depth 0))
-          (c-put-font-lock-face start (point) 'font-lock-comment-face))))) nil)
 
 (defun fix-patch-paths ()
-  "Fix paths in patch files made on windows (fix slashes)."
+  "Fix paths in patch files made on windows (fix slashes) (cvs diff)."
   (interactive)
   (save-excursion
-	(goto-char (point-min))
-	(while (re-search-forward "^\\(\\+\\+\\+\\|Index: \\|\\-\\-\\-\\|RCS file:\\).*$")
-	  (let ( (str (match-string)) )
-			(setq str (replace-regexp-in-string "/" "\\" (match-string 0) t t))
-			(replace-match str t t)))
-	(message "Done.")))
+    (goto-char (point-min))
+    (while (re-search-forward "^\\(\\+\\+\\+ |Index: \\|\\-\\-\\- \\|RCS file:\\).*$")
+      (let* ((str (match-string 0))
+			 (rep (replace-regexp-in-string "/" "\\" str t t)))
+		(replace-match rep t t)))
+    (message "Done.")))
 
 
 ;;;;;;;;;;;;;;;;;;;;
@@ -282,8 +213,8 @@
 (defun backward-kill-line ()
   "Kill from current point to the beginning of the current line."
   (interactive)
-  (while (not (bolp)) (delete-backward-char 1)))
-;  (delete-region () (point)))
+  (while (not (bolp))
+	(delete-backward-char 1)))
 ;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;
@@ -292,10 +223,10 @@
 ;  "Foo func!!"
 ;  (interactive)
 ;  (save-excursion
-;	(goto-char (point-min))
-;	; move around and do cool stuff...
-;	; ...
-;	(message "other stuff...")))
+;   (goto-char (point-min))
+;   ; move around and do cool stuff...
+;   ; ...
+;   (message "other stuff...")))
 ;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;
@@ -308,7 +239,7 @@ Argument END End of region.
 Argument REGEXP Regex to align on."
   (interactive "r\nsAlign regexp: ")
   (align-regexp start end
-				(concat "\\(\\s-*\\)" regexp) 1 1 t))
+                (concat "\\(\\s-*\\)" regexp) 1 1 t))
 
 ;;;;;;;;;;;;;;;;;;;;
 ;; print a new week in TODO
@@ -317,40 +248,117 @@ Argument REGEXP Regex to align on."
   "Print blank week schedule."
   (interactive)
   (save-excursion
-	(insert
-	 (format-time-string
+    (insert
+     (format-time-string
 "* Week %m/%d/%y
   * Mon
-	-
+    -
   * Tue
-	-
+    -
   * Wed
-	-
+    -
   * Thu
-	-
+    -
   * Fri
-	-
-	- Submit Time Card
+    -
 "))))
 
 
-(defun reinterpret-hex ()
-  "Show ASCII hex values in plain ASCII in another buffer."
-  (interactive)
-  (setq buf (get-buffer-create "*reinterpret-hex*"))
-  (save-current-buffer ; clear the temp buffer
-	(set-buffer buf)
-	(erase-buffer))
-  (save-excursion
-	(let ((end (if mark-active (region-end) (point-max))))
-	  (goto-char (if mark-active (region-beginning) (point-min)))
+(defun reinterpret-hex (start end)
+  "Show ASCII hex values in plain ASCII in another buffer.
+Looks through entire buffer if no region is selected.
+Argument START .
+Argument END End of region."
+  (interactive "r")
+  (let ((buf (get-buffer-create "*reinterpret-hex*")))
+	(with-current-buffer buf (erase-buffer))
+	(save-excursion
+	  (goto-char start)
 	  (while (search-forward-regexp "\\([0-9a-fA-F]\\{2\\}\\)" end t)
 		(let ((m1 (match-string 1)))
-		  (save-current-buffer
-			(set-buffer buf)
+		  (with-current-buffer buf
 			(insert (format "%s: %c\n" m1 (string-to-number m1 16)))))))
+	(display-buffer buf)))
+
+(defun extract-wrs (start end sep)
+  "Search for wr numbers in a region.
+Results are are displayed in the *WRS* buffer.
+Argument START Start of region.
+Argument END End of region.
+Argumen SEP Output seperator"
+  (interactive "r\nsSeperator: ")
+  (let ((buf (get-buffer-create "*WRS*")))
+	(with-current-buffer buf (erase-buffer))
+	(save-excursion
+	  (goto-char start)
+;	  (while (search-forward-regexp "[0-9]\\{4,5\\}" end t) ;tends to pick up years...
+	  (while (search-forward-regexp "[0-9]\\{5\\}" end t)
+		(let ((str (match-string 0)))
+		  (with-current-buffer buf
+			(insert str sep)))))
+	(display-buffer buf)))
+
+
+; (defun keep-matches-only (regexp)
+;   "Given a regex, delete everything in buffer except matches"
+;   (interactive "sRegexp: ")
+;   (save-excursion
+; 	(goto-char (point-min))
+; 	(while (not (eobp))
+; 	  (let ((cur (point)))
+; 		(search-forward-regexp regexp)
+; 		(kill-region cur (match-beginning 0))))))
+
+
+; based on sample from documentation for `sort-subr'
+(defun sort-lines (reverse beg end)
+  "Sort lines in region by length"
+  (interactive "P\nr")
+  (save-excursion
+	(save-restriction
+	  (narrow-to-region beg end)
+	  (goto-char (point-min))
+	  (let ((inhibit-field-text-motion t))
+		(sort-subr reverse 'forward-line 'end-of-line 
+				   (lambda ()
+					 (save-excursion
+					   (beginning-of-line)
+					   (let ((strt (point)))
+						 (end-of-line)
+						 (- (point) strt)))))))))
+
+; insert c decl string
+(defun c-ins-fname ()
+  "insert current function name at point"
+  (interactive)
+  (insert (c-defun-name)))
+
+; insert c decl string comment
+(defun c-ins-fname-decl ()
+  "insert current function name at point"
+  (interactive)
+  (insert (format "static const char name[] = \"%s\";" (c-defun-name))))
+
+; run calc on region
+(defun calc-region (beg end)
+  (interactive "r")
+  (when (region-active-p)
+	(calc-embedded nil)
+	(calc-embedded nil)
+	(message "Calculated")))
+
+(defun keepers (beg end re)
+  (interactive "r\ne")
+  (let ((buf (get-buffer-create "*KEEPERS*")))
+	(save-excursion
+	  (goto-char start)
+	  (while (search-forward-regexp re end t)
+		(let ((str (match-string 0)))
+		  (with-current-buffer buf
+			(insert str "\n")))))
 	(display-buffer buf)))
 
 (provide 'funcs)
 
 ;;; funcs.el ends here
+
